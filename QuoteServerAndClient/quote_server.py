66b39654d1d4e68c63e12c184c31f3147998f0ec
Vladislav Stognienko <stognienkovv@gmail.com>
2018-03-30 14:19:47 +0300

    quote servr_client

--- QuoteServerAndClient/quote_server.py
+++ QuoteServerAndClient/quote_server.py
@@ -14,3 +14,28 @@ class QuoteProtocol(protocol.Protocol):
         print("Number of active connections: {}".format(self.factory.numConnections))
         print("> Received: ``{}''\n> Sending: ``{}''".format(data, self.getQuote()))
         self.transport.write(self.getQuote())
+        self.updateQuote(data)
+
+    def connectionLost(self, reason):
+        self.factory.numConnections -= 1
+
+    def getQuote(self):
+        return self.factory.quote
+
+    def updateQuote(self, quote):
+        self.factory.quote = quote
+
+
+class QuoteFactory(Factory):
+    numConnections = 0
+    protocol = QuoteProtocol
+
+    def __init__(self, quote=None):
+        self.quote = quote or "An apple a day keeps the doctor away kek"
+
+    def buildProtocol(self, addr):
+        return QuoteProtocol(self)
+
+
+reactor.listenTCP(8000, QuoteFactory())
+reactor.run()
