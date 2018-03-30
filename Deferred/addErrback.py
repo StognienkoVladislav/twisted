5597c5132ea93f4262f955a47ac99aae07bf3ed6
Vladislav Stognienko <stognienkovv@gmail.com>
2018-03-30 15:07:55 +0300

    intro to deferred

---
+++ Deferred/addErrback.py
@@ -0,0 +1,11 @@
+
+from twisted.internet.defer import Deferred
+
+
+def myErrback(failure):
+    print(failure)
+
+
+d = Deferred()
+d.addErrback(myErrback)
+d.errback("Triggering errback.")
---
+++ Deferred/multiple_callbacks.py
@@ -0,0 +1,21 @@
+
+from twisted.internet.defer import Deferred
+
+
+def addBold(result):
+    return "<b>{}</b>".format(result)
+
+
+def addItalic(result):
+    return "<i>{}</>".format(result)
+
+
+def printHTML(result):
+    print(result)
+
+
+d = Deferred()
+d.addCallback(addBold)
+d.addCallback(addItalic)
+d.addCallback(printHTML)
+d.callback("Hello World")
